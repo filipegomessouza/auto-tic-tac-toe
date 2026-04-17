3310ae9af531f8244864715655641c12eeac361b
Filipe Gomes <filipe.gomes.arante@gmail.com>
2026-04-16 22:44:19 -0300

    feat: remove unused imports

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from src.enums.play_option import PlayOption
-from src.game.players.human_player import HumanPlayer
-from src.game.players.optimal_player import OptimalPlayer
-from src.game.players.random_player import RandomPlayer
 from src.game.tic_tac_toe_game import TicTacToeGame
 from src.exceptions.players_with_equal_play_option_exception import PlayersWithEqualPlayOptionException
 import config
